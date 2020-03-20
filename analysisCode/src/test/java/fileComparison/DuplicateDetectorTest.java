222434a16997bb0b142992d803ad5895cfc86e6d
AndrewC19 <agclark2@sheffield.ac.uk>
2020-03-20 10:17:39 +0000

    Removed week 5 test cases

--- analysisCode/src/test/java/fileComparison/DuplicateDetectorTest.java
+++ analysisCode/src/test/java/fileComparison/DuplicateDetectorTest.java
@@ -11,8 +11,6 @@ public class DuplicateDetectorTest {
 
     @Test
     public void testCompareFiles() throws IOException {
-        DuplicateDetector dd = new DuplicateDetector(new File("/Users/neil/Google Drive/Teaching/Sheffield/Reengineering/2019-20/Examples/weka-3.8/weka/src/main/java"), "java");
-        dd.fileComparison(new File("output.csv"));
     }
 
 }
--- analysisCode/src/test/java/fileComparison/FileComparatorTest.java
+++ analysisCode/src/test/java/fileComparison/FileComparatorTest.java
@@ -11,10 +11,6 @@ public class FileComparatorTest {
 
     @Test
     public void detailedComparisonTest() throws IOException {
-        FileComparator fc = new FileComparator(new File("/Users/neil/Google Drive/Teaching/Sheffield/Reengineering/2019-20/Examples/weka-3.8/weka/src/main/java/weka/gui/explorer/ClustererPanel.java"),new File("/Users/neil/Google Drive/Teaching/Sheffield/Reengineering/2019-20/Examples/weka-3.8/weka/src/main/java/weka/gui/explorer/ClassifierPanel.java"));
-        boolean[][] comparison = fc.detailedCompare();
-        //fc.printRelations(comparison,new File("output.csv"));
-        fc.comparisonBitmap(comparison,new File("output.png"));
     }
 
 }
